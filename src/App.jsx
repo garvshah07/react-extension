a529a2f483e58c2959d257be2495b857b8ec044e
garvshah07 <2104030100903@silveroakuni.ac.in>
2026-08-08 19:06:09 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <section>
-      <div className='flex justify-center w-[800px]'>
-        <h1>Garv Shah</h1>
-      </div>
-    </section>
+    <Main>
+  
+    </Main>
   )
 }
 
